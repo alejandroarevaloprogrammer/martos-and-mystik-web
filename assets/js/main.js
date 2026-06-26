7230a988046ed29a412c3aaedb21d6eec6c7b838
Alejandro Arevalo Rojas <alejandroarevalo.programmer@gmail.com>
2026-06-26 05:14:20 +0200

    Arrow for Top Up Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,25 @@ document.addEventListener("DOMContentLoaded", () => {
     renderReleases();
     updateFeaturedRelease();
 
+    const backToTopButton = document.querySelector("#backToTop");
+
+    if (backToTopButton) {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 500) {
+            backToTopButton.classList.add("is-visible");
+        } else {
+            backToTopButton.classList.remove("is-visible");
+        }
+    });
+
+    backToTopButton.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+}
+
     document.addEventListener("languageChanged", () => {
         renderReleases();
         updateFeaturedRelease();
